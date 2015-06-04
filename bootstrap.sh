7cc3fcaa2f97ea77f117963afd329711ac8ac59e
thiswind <thiswind@vip.qq.com>
2015-06-04 09:30:18 +0800

    daily

--- bootstrap.sh
+++ bootstrap.sh
@@ -17,22 +17,23 @@ fi
 
 . .venv/bin/activate
 
-pip install -q --upgrade pip
-pip install -q --upgrade flask-restful
-pip install -q --upgrade Flask-Testing
-pip install -q --upgrade pyOpenSSL
-pip install -q --upgrade flask-cors
-pip install -q --upgrade dnspython
-pip install -q --upgrade docker-py
+pip install --upgrade requests[security]
+pip install --upgrade pip
+pip install --upgrade flask-restful
+pip install --upgrade Flask-Testing
+pip install --upgrade pyOpenSSL
+pip install --upgrade flask-cors
+pip install --upgrade dnspython
+pip install --upgrade docker-py
 
 deactivate
 
 #gen keys
-KEY_HOME=".keys"
-mkdir -p ${KEY_HOME}
-sudo -H openssl genrsa -des3 -out ${KEY_HOME}/key.key 1024
-sudo -H openssl req -new -key ${KEY_HOME}/key.key -out ${KEY_HOME}//csr.csr
-sudo -H openssl x509 -req -days 365 -in ${KEY_HOME}/csr.csr -signkey ${KEY_HOME}/key.key -out ${KEY_HOME}/crt.crt
-sudo -H cp ${KEY_HOME}/key.key ${KEY_HOME}/key.key.bak
-sudo -H openssl rsa -in ${KEY_HOME}/key.key.bak -out ${KEY_HOME}/key.key
+#KEY_HOME=".keys"
+#mkdir -p ${KEY_HOME}
+#sudo -H openssl genrsa -des3 -out ${KEY_HOME}/key.key 1024
+#sudo -H openssl req -new -key ${KEY_HOME}/key.key -out ${KEY_HOME}//csr.csr
+#sudo -H openssl x509 -req -days 365 -in ${KEY_HOME}/csr.csr -signkey ${KEY_HOME}/key.key -out ${KEY_HOME}/crt.crt
+#sudo -H cp ${KEY_HOME}/key.key ${KEY_HOME}/key.key.bak
+#sudo -H openssl rsa -in ${KEY_HOME}/key.key.bak -out ${KEY_HOME}/key.key
 
